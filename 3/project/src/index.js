64cf4aee2484ef8cc923bd337411bcd938c51018
wali55 <walisantunu@gmail.com>
2023-09-19 18:58:25 +0600

    cli todo app prep

--- 3/project/src/index.js
+++ 3/project/src/index.js
@@ -4,6 +4,3 @@ const Todo = require('./Todo');
 const { saveFile, readFile } = require('./utils');
 const { ADD, UPDATE, NEXT, DONE, FIND, LIST } = require('./commands');
 
-(function init() {
-
-})();
