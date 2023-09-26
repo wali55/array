0ed8c62b20637d27bc1729a56d7e507e0434d303
wali55 <walisantunu@gmail.com>
2023-09-26 20:19:24 +0600

    forEach

--- 6/lec-1.js
+++ 6/lec-1.js
@@ -1,11 +1,24 @@
+/**/
 const names = [
     'wali',
     'hasib',
     'sakib'
 ];
 
-function reuse() {
-    for (let i = 0; i < names.length; i++) {
-        console.log(names);
+function reuse(arr, logic) {
+    for (let i = 0; i < arr.length; i++) {
+        logic(arr[i], i, arr);
     }
 }
+
+function logic1(value) {
+    console.log(value);
+}
+
+function logic(value, index, array) {
+    console.log(value, index, array);
+}
+
+reuse([1, 2], logic);
+reuse(['one', 'two'], logic1);
+// reuse(names);
